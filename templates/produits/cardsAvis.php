84c5785cdbe4a7b0e26a557e7adc94a95d3e1037
nabil13090 <nabil.chetouane13090@gmail.com>
2024-05-29 09:24:31 +0200

    responsive avis

--- templates/produits/cardsAvis.php
+++ templates/produits/cardsAvis.php
@@ -7,20 +7,18 @@ use Models\Produit;
 $mesAvis = new Produit();
 $avis = $mesAvis->findAvis();
 ?>
-<div class="container mt-5 mb-5 border border-black rounded" style="max-width: 800px;">
+<div class="container mt-5 mb-6" style="max-width: 1200px; margin-bottom: 3rem;">
     <h3 class="text-center my-4">LES AVIS CLIENTS</h3>
-    <div class="row mb-5">
+    <div class="d-flex flex-wrap justify-content-center">
         <?php foreach ($avis as $value) { ?>
-            <div class="col-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center">
-                <div class="card p-2 py-3 text-center" style="width: 200px;">
-                    <div class="img mb-2 ratio ratio-1x1">
-                        <img src="../../<?= $value['image'] ?>" class="rounded-circle img-fluid object-fit-cover" alt="<?= $value['nom'] ?>">
-                    </div>
-                    <h5 class="mb-0"><?= $value['nom'] ?></h5>
-                    <div class="ratings d-flex mx-auto"><?php $mesAvis->getStar($value['rating']); ?></div>
-                    <div class="mt-1 apointment">
-                        <p><?= $value['desciption'] ?></p>
-                    </div>
+            <div class="card p-2 py-3 text-center m-2" style="width: 200px;">
+                <div class="img mb-2 ratio ratio-1x1">
+                    <img src="../../<?= $value['image'] ?>" class="rounded-circle img-fluid object-fit-cover" alt="<?= $value['nom'] ?>">
+                </div>
+                <h5 class="mb-0"><?= $value['nom'] ?></h5>
+                <div class="ratings d-flex mx-auto"><?php $mesAvis->getStar($value['rating']); ?></div>
+                <div class="mt-1 apointment">
+                    <p><?= $value['desciption'] ?></p>
                 </div>
             </div>
         <?php } ?>
